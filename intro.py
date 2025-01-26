c42f3dd073b0bde8aeef21703b2115614d738a12
NefePress <jkt.nefe@gmail.com>
2025-01-26 20:03:14 +0100

    our intro to python class

---
+++ intro.py
@@ -0,0 +1,40 @@
+x = 10
+print(x)
+
+# # integer
+# age = 30
+# # strings
+# designation = "DevOps engineer"
+# # float
+# price = 99.99
+# # boolean
+# is_loggedin = True
+# # dictionaries - they are key value pairs
+# user_info = {"name": "John" , "age": 20}
+# # list  - comes with []
+# groceries = ["towel" , "soaps"]
+# # tuples - the () are optional
+# even_numbers = (2, 4 , 6 , 8)
+
+# control structures
+# 1. if statements
+# 2. elseif
+# 3. else 
+
+y = 20
+if x > y:
+    print ("x is greater than y")
+elif x < y:
+    print ("x is less than y")
+else:
+    print ("x equal y")
+
+
+# loop
+# for - loop
+# while - loops
+
+count = 0
+while count < 5:
+    print (count)
+    count += 1
